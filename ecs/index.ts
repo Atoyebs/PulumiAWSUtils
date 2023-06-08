b1ac9dfd4ad5b52fdeded94dc1b9412a3abdcee2
Ini Atoyebi <iatoyebi.3134@gmail.com>
2023-06-08 02:21:40 +0100

    another try

--- ecs/index.ts
+++ ecs/index.ts
@@ -1,7 +1,7 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 import * as awsx from "@pulumi/awsx";
-import { CombinedFargateResource, PORTS } from "../types";
+import { CombinedFargateResource, PORTS } from "../typedefs";
 
 interface CombinedResourceItem {
   targetGroup?: aws.lb.TargetGroup;
